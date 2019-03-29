7e1f257a5878a59cdf52eaeed29997e580b51988
韬韫 <609448234@qq.com>
2019-03-29 21:12:50 +0800

    feat: use umd

--- src/index.js
+++ src/index.js
@@ -1,69 +1,77 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * step: init => start => init...
- *
- * @enum {number}
- */
-var Status;
-(function (Status) {
-    Status[Status["init"] = 0] = "init";
-    Status[Status["start"] = 1] = "start";
-})(Status = exports.Status || (exports.Status = {}));
-var matching = function (str, option) {
-    if (str === void 0) { str = ''; }
-    var _a = option.startChar, startChar = _a === void 0 ? '' : _a, _b = option.endChar, endChar = _b === void 0 ? '' : _b, cb = option.cb, _c = option.isGreedy, isGreedy = _c === void 0 ? false : _c;
-    var strList = [];
-    var tempStrArr = [];
-    var status = Status.init;
-    var push = function (trigger) {
-        if (trigger === void 0) { trigger = false; }
-        var str = tempStrArr.join('');
-        if (!str) {
-            return;
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    /**
+     * step: init => start => init...
+     *
+     * @enum {number}
+     */
+    var Status;
+    (function (Status) {
+        Status[Status["init"] = 0] = "init";
+        Status[Status["start"] = 1] = "start";
+    })(Status = exports.Status || (exports.Status = {}));
+    const matching = (str = '', option) => {
+        const { startChar = '', endChar = '', cb, isGreedy = false } = option;
+        let strList = [];
+        let tempStrArr = [];
+        let status = Status.init;
+        const push = (trigger = false) => {
+            let str = tempStrArr.join('');
+            if (!str) {
+                return;
+            }
+            if (trigger && cb) {
+                str = cb(str);
+            }
+            strList.push(str);
+            tempStrArr = [];
+        };
+        for (let i = 0; i < str.length; i++) {
+            const length = Status.init ? startChar.length : endChar.length;
+            const val = str.slice(i, i + length);
+            i += (length - 1);
+            if (val === startChar && status === Status.init) {
+                status = Status.start;
+                push();
+                continue;
+            }
+            if (val === endChar && status === Status.start) {
+                if (isGreedy) {
+                    push(true);
+                    const [first, ...content] = strList;
+                    strList = [first, content.join('')];
+                    tempStrArr.push(val);
+                }
+                else {
+                    push(true);
+                    status = Status.init;
+                }
+                continue;
+            }
+            tempStrArr.push(val);
         }
-        if (trigger && cb) {
-            str = cb(str);
+        if (isGreedy) {
+            tempStrArr.splice(0, 1);
         }
-        strList.push(str);
-        tempStrArr = [];
+        push();
+        return strList;
     };
-    for (var i = 0; i < str.length; i++) {
-        var length_1 = Status.init ? startChar.length : endChar.length;
-        var val = str.slice(i, i + length_1);
-        i += (length_1 - 1);
-        if (val === startChar && status === Status.init) {
-            status = Status.start;
-            push();
-            continue;
-        }
-        if (val === endChar && status === Status.start) {
-            if (isGreedy) {
-                push(true);
-                var first = strList[0], content = strList.slice(1);
-                strList = [first, content.join('')];
-                tempStrArr.push(val);
-            }
-            else {
-                push(true);
-                status = Status.init;
-            }
-            continue;
-        }
-        tempStrArr.push(val);
+    /*
+    export const matchingByRegExp: Matching = (str = '', option) => {
+      const { startChar = '', endChar = '', cb, isGreedy = false } = option
+    
+      const regexp = new RegExp(`${startChar}(\\S*${isGreedy ? '?' : ''})${endChar}`)
+      return str.split(regexp).filter(x => !!x)
     }
-    if (isGreedy) {
-        tempStrArr.splice(0, 1);
-    }
-    push();
-    return strList;
-};
-/*
-export const matchingByRegExp: Matching = (str = '', option) => {
-  const { startChar = '', endChar = '', cb, isGreedy = false } = option
-
-  const regexp = new RegExp(`${startChar}(\\S*${isGreedy ? '?' : ''})${endChar}`)
-  return str.split(regexp).filter(x => !!x)
-}
-*/
-exports.default = matching;
+    */
+    exports.default = matching;
+});
